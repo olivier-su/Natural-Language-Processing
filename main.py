2345d8f07bae86576c719f2d872ea08fa4e83d50
HWJFish <wwc641810324@gmail.com>
2021-12-01 16:27:50 -0500

    added code to generate details.csv and missing analysis.csv; probably should refactor using vector

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 import gensim.downloader as api
 import pandas as pd
+import numpy as np
+import csv
 
-model = api.load("word2vec-google-news-300")
-print(api.info("word2vec-google-news-300"))
-synonyms = pd.read_csv('synonyms.csv')
 
+def evaluate_model(model_name:str):
+    model = api.load(model_name)
+    synonyms_file = pd.read_csv('synonyms.csv')
+    synonyms = synonyms_file.to_numpy()
+    output_file=open(f'{model_name}-details.csv','w',newline='')
+    writer=csv.writer(output_file)
+    writer.writerow(['Question','Correct-answer','Guess-word','Correctness'])
+    for synonym in synonyms:
+        if synonym[0] in model.key_to_index:
+            similarity = model.most_similar(synonym[0])
+            # for testing only
+            print(similarity)
+
+            guess=similarity[0][0]
+            correctness= ''
+            if synonym[1]==guess:
+                correctness='correct'
+            else:
+                if guess in[synonym[2],synonym[3],synonym[4],synonym[5]]:
+                    correctness='wrong'
+                else:
+                    correctness='guess'
+            writer.writerow([synonym[0], synonym[1], guess, correctness])
+        else:
+            writer.writerow([synonym[0], synonym[1],"NA","guess"])
+
+
+
+
+
+
+
+
+
+def main():
+    evaluate_model("word2vec-google-news-300")
+
+
+if __name__ == '__main__':
+    main()
